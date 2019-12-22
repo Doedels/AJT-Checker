5ce47211605b7d54db5d6bc6286361a9c6cb2dfb
Doedels <sandermarkies@yahoo.com>
2019-12-22 21:21:28 +0100

    Toevoegen useState Hook, input+handler

--- src/Containers/FrontPage/FrontPage.tsx
+++ src/Containers/FrontPage/FrontPage.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 type FrontPageProps = {
   text: string;
+  number?: number;
 };
 
-const frontPage = (props: FrontPageProps) => {
+const FrontPage = (props: FrontPageProps) => {
+  const [partner, setPartner] = useState(false);
+
+  const partnerChangedHandler = (event: any) => {
+    setPartner(event.target.checked);
+  };
+
   return (
     <div>
       <h1>{props.text}</h1>
+      <input type="checkbox" onChange={partnerChangedHandler} />
+      <p>Partner = {`${partner}`}</p>
     </div>
   );
 };
 
-export default frontPage;
+export default FrontPage;
