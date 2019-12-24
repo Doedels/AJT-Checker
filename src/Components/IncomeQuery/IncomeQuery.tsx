b7e010f8bd557576907e2cdff4ba173dd01cfc1b
Doedels <sandermarkies@yahoo.com>
2019-12-24 11:40:14 +0100

    Toevoegen InqomeQuery Component

---
+++ src/Components/IncomeQuery/IncomeQuery.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type IncomeQueryProps = {
+  incomeArray: string[];
+};
+
+const IncomeQuery = (props: IncomeQueryProps) => {
+  return (
+    <div>
+      <p>IncomeQuery</p>
+    </div>
+  );
+};
+
+export default IncomeQuery;
